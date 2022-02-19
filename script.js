cbe83ecd4ff07d1ab97368e9ee33acc907a1e6f7
Sabarish-Chandrasekaran <100018763+Sabarish-Chandrasekaran@users.noreply.github.com>
2022-02-19 19:33:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+//Execute the below code and write your description in txt file
+console.log(typeof(1));
+console.log(typeof(1.1));
+console.log(typeof('1.1'));
+console.log(typeof(true));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(typeof([]));
+console.log(typeof({}));
+console.log(typeof(NaN));
